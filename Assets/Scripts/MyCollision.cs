f9511a74a94e253e6a0a1ce3211dc0f7b65eca31
test <pavolleopold@gmail.com>
2015-10-13 22:10:43 +0200

    upload of group work

---
+++ Assets/Scripts/MyCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCollision : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        transform.position = new Vector3(30, -1, -15);
+
+       
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rotation : MonoBehaviour {
+
+    /*[Range(-100,100)]*/ public float Xaxis = 0;
+    /*[Range(-100,100)]*/ public float Yaxis = 0;
+    /*[Range(-100,100)]*/ public float Zaxis = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Xaxis, Yaxis, Zaxis);
+	}
+}
