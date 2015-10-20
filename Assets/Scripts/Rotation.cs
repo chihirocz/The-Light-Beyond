31175c251b56458e97e03dcb42756eee58a4ee7a
xpribo03 <xpribo03@stud.fit.vutbr.cz>
2015-10-20 02:36:37 +0200

    Collision + Time.deltaTime

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,18 +4,32 @@ using System.Collections;
 
 public class Rotation : MonoBehaviour {
 
-    /*[Range(-100,100)]*/ public float Xaxis = 0;
-    /*[Range(-100,100)]*/ public float Yaxis = 0;
-    /*[Range(-100,100)]*/ public float Zaxis = 0;
+    /*[Range(-100,100)]*/
+    public float Xaxis = 0;
+    /*[Range(-100,100)]*/
+    public float Yaxis = 0;
+    /*[Range(-100,100)]*/
+    public float Zaxis = 0;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Xaxis, Yaxis, Zaxis);
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+       // Debug.Log("collision");
+        Xaxis = -Xaxis;
+        Yaxis = -Yaxis;
+        Zaxis = -Zaxis;
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Xaxis * Time.deltaTime, Yaxis * Time.deltaTime, Zaxis * Time.deltaTime);
 	}
 }
+
