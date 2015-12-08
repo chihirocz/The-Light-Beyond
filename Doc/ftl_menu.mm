dd83a0b9be9e860cd900a15d7299de573c98aede
chihirocz <pursuer@seznam.cz>
2015-12-08 18:55:42 +0100

    game menu graph (mindmap)

---
+++ Doc/ftl_menu.mm
@@ -0,0 +1,69 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="new game&#xa;options&#xa;credits&#xa;exit" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1449509620031"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="NEW GAME&#xa;campaign&#xa;random level&#xa;tutorial&#xa;back" POSITION="right" ID="ID_1311616412" CREATED="1449509593169" MODIFIED="1449509732786">
+<edge COLOR="#ff0000"/>
+<node TEXT="RANDOM LEVEL&#xa;easy&#xa;medium&#xa;hard&#xa;back" ID="ID_718665804" CREATED="1449510033289" MODIFIED="1449510087396"/>
+<node TEXT="TUTORIAL&#xa;levels&#xa;back" ID="ID_1499257493" CREATED="1449510088994" MODIFIED="1449510891929"/>
+</node>
+<node TEXT="CREDITS&#xa;nase jmena&#xa;back" POSITION="right" ID="ID_104864515" CREATED="1449510915859" MODIFIED="1449510925648">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="OPTIONS&#xa;sound (volume)&#xa;music (volume)&#xa;(resolution)&#xa;(freelook/yuv)&#xa;keys mapping&#xa;back" POSITION="left" ID="ID_398547287" CREATED="1449509744493" MODIFIED="1449509987288">
+<edge COLOR="#0000ff"/>
+<node TEXT="KEYS MAPPING&#xa;forward&#xa;backward&#xa;use powerup&#xa;(&#xa;up,&#xa;down,&#xa;left,&#xa;right&#xa;)&#xa;back" ID="ID_1766743407" CREATED="1449509957559" MODIFIED="1449511214867"/>
+</node>
+<node TEXT="EXIT&#xa;yes&#xa;no" POSITION="left" ID="ID_1156807998" CREATED="1449510936701" MODIFIED="1449510969575">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
